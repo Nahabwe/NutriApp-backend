e913002a7af87d2df0c938bce422093fa4f7cd04
nahabwe <nahabwesirmuel@gmail.com>
2025-05-30 13:27:23 +0300

    Added logging

--- diabetes_backend/settings.py
+++ diabetes_backend/settings.py
@@ -5,6 +5,8 @@ from datetime import timedelta
 import dj_database_url
 import os
 import environ
+import logging
+
 env =environ.Env(
     DEBUG=(bool,False)
 )
@@ -24,7 +26,19 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
 
 
-
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
 
 # Application definition
 
